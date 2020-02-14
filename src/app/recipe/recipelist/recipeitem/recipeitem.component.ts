c2d549eea63d29a5280d83cd5716a9fb66bea51a
pasiem <emily_pasiak@hotmail.com>
2020-02-14 11:37:02 +0100

    modularized recipeItem

--- src/app/recipe/recipelist/recipeitem/recipeitem.component.ts
+++ src/app/recipe/recipelist/recipeitem/recipeitem.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipeitem',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipeitem.component.css']
 })
 export class RecipeitemComponent implements OnInit {
+  @Input() recipe: Recipe;
 
   constructor() { }
 
